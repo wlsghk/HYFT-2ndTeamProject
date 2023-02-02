1d33aa95d96c9b486d81925d3c00ee28dae2fa85
배진화 <baevolutiond@gmail.com>
2023-02-02 19:01:06 +0900

    modify

--- index.js
+++ index.js
@@ -307,3 +307,85 @@ $topBtn.addEventListener("click", (e) => {
     behavior: "smooth",
   });
 });
+
+/* smooth scroll
+class Scrooth {
+  constructor({
+    element = window,
+    strength = 10,
+    acceleration = 1.2,
+    deceleration = 0.975,
+  } = {}) {
+    this.element = element;
+    this.distance = strength;
+    this.acceleration = acceleration;
+    this.deceleration = deceleration;
+    this.running = false;
+
+    this.element.addEventListener("wheel", this.scrollHandler.bind(this), {
+      passive: false,
+    });
+    this.element.addEventListener("mousewheel", this.scrollHandler.bind(this), {
+      passive: false,
+    });
+    this.scroll = this.scroll.bind(this);
+  }
+
+  scrollHandler(e) {
+    e.preventDefault();
+
+    if (!this.running) {
+      this.top = this.element.pageYOffset || this.element.scrollTop || 0;
+      this.running = true;
+      this.currentDistance = e.deltaY > 0 ? 0.1 : -0.1;
+      this.isDistanceAsc = true;
+      this.scroll();
+    } else {
+      this.isDistanceAsc = false;
+      this.currentDistance = e.deltaY > 0 ? this.distance : -this.distance;
+    }
+  }
+
+  scroll() {
+    if (this.running) {
+      this.currentDistance *=
+        this.isDistanceAsc === true ? this.acceleration : this.deceleration;
+      Math.abs(this.currentDistance) < 0.1 && this.isDistanceAsc === false
+        ? (this.running = false)
+        : 1;
+      Math.abs(this.currentDistance) >= Math.abs(this.distance)
+        ? (this.isDistanceAsc = false)
+        : 1;
+
+      this.top += this.currentDistance;
+      this.element.scrollTo(0, this.top);
+
+      requestAnimationFrame(this.scroll);
+    }
+  }
+}
+
+const scroll = new Scrooth({
+  element: window,
+  strength: 20,
+  acceleration: 1.5,
+  deceleration: 0.975,
+}); */
+
+/* 스크롤 시 깜빡이는 애니메이션 */
+(function () {
+  let observer = new IntersectionObserver((e) => {
+    e.forEach((box) => {
+      if (box.isIntersecting) {
+        box.target.style.opacity = 1;
+      } else {
+        box.target.style.opacity = 0;
+      }
+    });
+  });
+
+  let $animation = document.querySelectorAll(".amation");
+  for (let i = 0; i < $animation.length; i++) {
+    observer.observe($animation[i]);
+  }
+})();
